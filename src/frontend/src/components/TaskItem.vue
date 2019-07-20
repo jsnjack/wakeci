80880c40847b529e7f1a19f3e61df564dac07aa0
Yauhen Shulitski <jsnjack@gmail.com>
2019-07-20 14:03:00 +0200

    :p: Monospace code displaying

--- src/frontend/src/components/TaskItem.vue
+++ src/frontend/src/components/TaskItem.vue
@@ -10,8 +10,8 @@
         <BuildStatus :status="status"></BuildStatus>
       </div>
     </div>
-    <div class="log-container text-left">
-      <span v-for="item in task.logs" :key="item.id" class="d-block">{{ item.data }}</span>
+    <div class="log-container text-left code">
+      <pre v-for="item in task.logs" :key="item.id" class="d-block">{{ item.data }}</pre>
     </div>
   </section>
 </template>
@@ -67,8 +67,9 @@ export default {
 .log-container {
   background: $bg-color;
   margin-left: 1em;
-  span {
+  pre {
       padding-left: 1em;
+      margin: 0;
   }
 }
 
