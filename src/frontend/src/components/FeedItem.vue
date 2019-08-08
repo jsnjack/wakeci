0a1e00700636a5f7e777b1439436b7afc28761b3
Yauhen Shulitski <jsnjack@gmail.com>
2019-08-08 21:19:05 +0200

    :m: Add retry button to feed page

--- src/frontend/src/components/FeedItem.vue
+++ src/frontend/src/components/FeedItem.vue
@@ -18,7 +18,37 @@
     </td>
     <td class="item-actions">
       <router-link :to="{ name: 'build', params: { id: build.id}}" class="btn btn-primary">Open</router-link>
+      <a href="#" @click.prevent="toggleRetryModal" class="btn btn-success">Retry</a>
       <a v-if="!isDone" :href="getAbortURL" @click.prevent="abort" class="btn btn-error">Abort</a>
+
+      <div class="modal" v-bind:class="{active: retryModalOpen}">
+        <a href="#" @click.prevent="toggleRetryModal" class="modal-overlay" aria-label="Close"></a>
+        <div class="modal-container">
+          <div class="modal-header">
+            <a
+              href="#"
+              @click.prevent="toggleRetryModal"
+              class="btn btn-clear float-right"
+              aria-label="Close"
+            ></a>
+            <div class="modal-title text-uppercase">{{ getRetryModalTitle }}</div>
+          </div>
+          <div class="modal-body">
+            <div class="content">
+              <form v-show="this.build.params" ref="form">
+                <RunFormItem v-for="item in build.params" :key="item.name" :params="item"></RunFormItem>
+              </form>
+              <div class="empty" v-show="!this.build.params">
+                <p class="empty-title h6 text-uppercase">Empty</p>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <a href="#" @click.prevent="retry" class="btn btn-primary float-right">Add to queue</a>
+          </div>
+        </div>
+      </div>
+
     </td>
   </tr>
 </template>
@@ -26,12 +56,13 @@
 <script>
 import BuildStatus from "@/components/BuildStatus";
 import BuildProgress from "@/components/BuildProgress";
+import RunFormItem from "@/components/RunFormItem";
 import Duration from "@/components/Duration";
 import axios from "axios";
 import {APIURL} from "@/store/communication";
 
 export default {
-    components: {BuildStatus, BuildProgress, Duration},
+    components: {BuildStatus, BuildProgress, Duration, RunFormItem},
     props: {
         build: {
             type: Object,
@@ -73,6 +104,12 @@ export default {
             }
             return "";
         },
+        isRetryModalOpen: function() {
+            return this.retryModalOpen;
+        },
+        getRetryModalTitle: function() {
+            return `${this.build.name} job parameters`;
+        },
     },
     methods: {
         abort(event) {
@@ -85,6 +122,31 @@ export default {
                 })
                 .catch((error) => {});
         },
+        retry(event) {
+            this.toggleRetryModal();
+            const url =
+        `${APIURL}/job/${this.build.name}/run?` +
+        new URLSearchParams(
+            Array.from(new FormData(this.$refs.form))
+        ).toString();
+            axios
+                .post(url)
+                .then((response) => {
+                    this.$notify({
+                        text: `${this.build.name} has been scheduled (#${response.data})`,
+                        type: "success",
+                    });
+                })
+                .catch((error) => {});
+        },
+        toggleRetryModal(event) {
+            this.retryModalOpen = !this.retryModalOpen;
+        },
+    },
+    data: function() {
+        return {
+            retryModalOpen: false,
+        };
     },
 };
 </script>
