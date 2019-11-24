30bc8758ea88ef9f8e58f4a85e97bae03e299389
Yauhen Shulitski <jsnjack@gmail.com>
2019-11-24 23:34:50 +0100

    :m: Speed up log reloading
    Do not use reactive vue elements per log string - very slow and cpu intensive, noticeable even on log files of few hundred kb :(

--- src/frontend/src/components/TaskItem.vue
+++ src/frontend/src/components/TaskItem.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div class="log-container text-left code">
-      <pre v-for="item in sortedLogs" :key="item.id" class="d-block log-line">{{ item.data }}</pre>
+      <pre class="d-block log-line">{{ content }}</pre>
     </div>
   </section>
 </template>
@@ -26,7 +26,6 @@
 <script>
 import BuildStatus from "@/components/BuildStatus";
 import Duration from "@/components/Duration";
-import {findInContainer} from "@/store/utils";
 import axios from "axios";
 
 export default {
@@ -55,17 +54,6 @@ export default {
         getDividerText: function() {
             return `task #${this.task.id}`;
         },
-        sortedLogs: function() {
-            return [...this.logItems].sort((a, b) => {
-                if (a.id > b.id) {
-                    return 1;
-                }
-                if (a.id < b.id) {
-                    return -1;
-                }
-                return 0;
-            });
-        },
         isVisible: function() {
             // Show only "main" tasks or tasks that were started. For example,
             // there is no need to show "finished" tasks if build failed because
@@ -90,34 +78,31 @@ export default {
                 .get(this.getLogURL)
                 .then((response) => {
                     this.$notify({
-                        text: "Reloading logs...",
+                        text: "Log file has been reloaded",
                         type: "success",
                         duration: 1000,
                     });
-                    response.data.split("\n").forEach((element, index) => {
-                        this.addLog({
-                            id: index,
-                            data: element,
+                    this.content = response.data;
+                    if (this.follow) {
+                        this.$nextTick(() => {
+                            this.$el.scrollIntoView(false);
                         });
-                    });
+                    }
                 })
                 .catch((error) => {});
         },
         addLog(log) {
-            const index = findInContainer(this.logItems, "id", log.id);
-            if (index[0] === undefined) {
-                this.logItems.push(log);
-                if (this.follow) {
-                    this.$nextTick(() => {
-                        this.$el.scrollIntoView(false);
-                    });
-                }
+            this.content = this.content + log.data;
+            if (this.follow) {
+                this.$nextTick(() => {
+                    this.$el.scrollIntoView(false);
+                });
             }
         },
     },
     data: function() {
         return {
-            logItems: [],
+            content: "",
         };
     },
 };
