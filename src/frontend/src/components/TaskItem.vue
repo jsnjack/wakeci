bf1bd16b0e26b3a4dfbfcb40722285c636d5efed
Yauhen Shulitski <jsnjack@gmail.com>
2020-03-18 21:58:26 +0100

    :p: Add code-block border color

--- src/frontend/src/components/TaskItem.vue
+++ src/frontend/src/components/TaskItem.vue
@@ -3,7 +3,7 @@
     <div class="divider" :data-content="getDividerText"></div>
     <div class="columns">
       <div class="column">
-        <div class="task-header text-left" :class="getBorderClass">
+        <div class="text-left">
           <BuildStatus :status="task.status"></BuildStatus>
           <span class="h5 task-name" @click="reloadLogs">{{ name }}</span>
           <Duration v-show="task.status !== 'pending'" :item="task" class="text-small m-1"></Duration>
@@ -29,7 +29,7 @@
         </div>
       </div>
     </div>
-    <div class="log-container text-left code">
+    <div class="log-container text-left code status-border" :class="getBorderClass">
       <pre v-if="content" class="d-block log-line">{{ content }}</pre>
     </div>
   </section>
@@ -87,7 +87,7 @@ export default {
             );
         },
         getBorderClass() {
-            return `border-${this.task.kind}`;
+            return `border-${this.task.status}`;
         },
         getLogURL() {
             return `/storage/build/${this.buildID}/task_${this.task.id}.log`;
@@ -179,7 +179,7 @@ export default {
 
 .log-container {
   background: $bg-color;
-  margin-left: 1em;
+  margin-left: 0.4em;
   overflow: auto;
   font-size: 90%;
   pre {
@@ -204,13 +204,10 @@ section {
   margin-bottom: 2em;
 }
 
-.task-header {
-  border-left: 0.2em solid;
-  padding-left: 0.4em;
+.status-border {
+    border-left: 0.25em solid;
 }
-.border-main {
-  border-left-color: $primary-color;
-}
+
 .border-pending {
   border-left-color: $gray-color;
 }
@@ -218,7 +215,7 @@ section {
   border-left-color: $warning-color;
 }
 .border-aborted {
-  border-left-color: $secondary-color;
+  border-left-color: $primary-color;
 }
 .border-failed {
   border-left-color: $error-color;
@@ -226,9 +223,7 @@ section {
 .border-finished {
   border-left-color: $success-color;
 }
-.border-finally {
-  border-left-color: $dark-color;
-}
+
 .task-name {
     cursor: pointer;
 }
