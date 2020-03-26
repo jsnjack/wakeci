c4894cff1a7c08ac19a7e0485783d99ead7fcf9d
Yauhen Shulitski <jsnjack@gmail.com>
2020-03-26 22:06:37 +0100

    :m: Secure tls configuration

--- src/backend/main.go
+++ src/backend/main.go
@@ -184,6 +184,17 @@ func main() {
 		server := &http.Server{
 			Addr: ":443",
 			TLSConfig: &tls.Config{
+				// https://ssl-config.mozilla.org/#server=golang&version=1.13.6&config=intermediate&guideline=5.4
+				MinVersion:               tls.VersionTLS12,
+				PreferServerCipherSuites: false,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				},
 				GetCertificate: certManager.GetCertificate,
 			},
 			Handler: gziphandler.GzipHandler(router),
