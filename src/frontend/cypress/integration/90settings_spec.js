6cd3be80ed675a1ffa6a54930f4e86236eeddd3b
Yauhen Shulitski <jsnjack@gmail.com>
2020-06-29 00:17:04 +0200

    :p: Improve tests stability

--- src/frontend/cypress/integration/90settings_spec.js
+++ src/frontend/cypress/integration/90settings_spec.js
@@ -2,6 +2,7 @@ describe("Settings page", function() {
     it("should modify settings", function() {
         cy.visit("/settings");
         cy.login();
+        cy.location("pathname").should("eq", "/settings");
         cy.get("input[type=password]").clear();
         cy.get("input[type=password]").type("secret");
 
@@ -12,10 +13,12 @@ describe("Settings page", function() {
         cy.get("[data-cy=save-settings]").click();
         cy.get(".notification-content").should("contain", "Saved");
         cy.get("[data-cy=logout]").click();
+        cy.location("pathname").should("eq", "/login");
 
         cy.login("secret");
 
         cy.visit("/settings");
+        cy.location("pathname").should("eq", "/settings");
         cy.get(".loading").should("not.exist");
         cy.get("input[type=password]").clear();
         cy.get("input[type=password]").type("admin");
