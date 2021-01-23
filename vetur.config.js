d6499b14d67bc56bc65e36fdeb0d399366fb9c62
Yauhen Shulitski <jsnjack@gmail.com>
2021-01-23 13:54:06 +0100

    Add vetur config

---
+++ vetur.config.js
@@ -0,0 +1,34 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    // **optional** default: `{}`
+    // override vscode settings
+    // Notice: It only affects the settings used by Vetur.
+    settings: {
+      "vetur.useWorkspaceDependencies": true,
+      "vetur.experimental.templateInterpolationService": true
+    },
+    // **optional** default: `[{ root: './' }]`
+    // support monorepos
+    projects: [
+      {
+        // **required**
+        // Where is your project?
+        // It is relative to `vetur.config.js`.
+        root: './src/frontend',
+        // **optional** default: `'package.json'`
+        // Where is `package.json` in the project?
+        // We use it to determine the version of vue.
+        // It is relative to root property.
+        package: './package.json',
+        // Register globally Vue component glob.
+        // If you set it, you can get completion by that components.
+        // It is relative to root property.
+        // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
+        globalComponents: [
+          './src/frontend/components/**/*.vue'
+        ],
+        tsconfig: null,
+      }
+    ]
+  }
