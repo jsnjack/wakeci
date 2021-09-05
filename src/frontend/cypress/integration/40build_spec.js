0d70e0b400f5267e33337b8e898577cbf25cb94a
Yauhen Shulitski <jsnjack@gmail.com>
2021-09-05 22:41:48 +0200

    Add tests

--- src/frontend/cypress/integration/40build_spec.js
+++ src/frontend/cypress/integration/40build_spec.js
@@ -251,5 +251,131 @@ artifacts:
             cy.get("[data-cy=openIndexFile]").should("have.attr", "href").and("contain", "bb");
         });
     });
+
+    it("should skip task when condition is false", function() {
+        // Create job
+        const jobName = "myjob" + new Date().getTime();
+        cy.request({
+            url: "/api/jobs/create",
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                "name": jobName,
+            },
+            form: true,
+        });
+
+        const jobContent = `
+desc: Condition test
+tasks:
+  - name: Print env
+    command: env
+    when: 1 == 2
+`;
+
+        cy.request({
+            url: "/api/job/" + jobName,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                "fileContent": jobContent,
+            },
+            form: true,
+        });
+
+        // Create build
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+
+        cy.visit("/");
+        cy.login();
+        cy.get("[data-cy=filter]").clear().type(jobName);
+        cy.get("tr").invoke("attr", "data-cy-build").then((val) => {
+            cy.get("[data-cy=open-build-button]").click();
+            cy.url().should("include", "/build/" + val);
+            cy.get("[data-cy=reload]").click();
+            cy.get(".notification-content").should("contain", "Log file has been reloaded");
+            cy.get("body").should("contain", "Condition is false");
+            cy.get("body").should("not.contain", "WAKE_BUILD_ID=");
+        });
+    });
+
+    it("should run task when condition is true", function() {
+        // Create job
+        const jobName = "myjob" + new Date().getTime();
+        cy.request({
+            url: "/api/jobs/create",
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                "name": jobName,
+            },
+            form: true,
+        });
+
+        const jobContent = `
+desc: Condition test
+params:
+ - NAME: joe
+tasks:
+  - name: Print env
+    command: env
+    when: $NAME == joe
+`;
+
+        cy.request({
+            url: "/api/job/" + jobName,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                "fileContent": jobContent,
+            },
+            form: true,
+        });
+
+        // Create build
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+
+        cy.visit("/");
+        cy.login();
+        cy.get("[data-cy=filter]").clear().type(jobName);
+        cy.get("tr").invoke("attr", "data-cy-build").then((val) => {
+            cy.get("[data-cy=open-build-button]").click();
+            cy.url().should("include", "/build/" + val);
+            cy.get("[data-cy=reload]").click();
+            cy.get(".notification-content").should("contain", "Log file has been reloaded");
+            cy.get("body").should("contain", "Condition is true");
+            cy.get("body").should("contain", "WAKE_BUILD_ID=");
+        });
+    });
 })
 ;
