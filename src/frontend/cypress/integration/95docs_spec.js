858686a99cd772a434c06a47dc5e31a3586f28fd
Yauhen Shulitski <jsnjack@gmail.com>
2022-01-30 18:05:48 +0100

    Add docs tests

---
+++ src/frontend/cypress/integration/95docs_spec.js
@@ -0,0 +1,13 @@
+describe("Docs menu", function() {
+    it("should open API docs", function() {
+        cy.visit("/docs/api/");
+        cy.get("body").contains("wakeci API documentation");
+    });
+
+    it("should open syntax docs", function() {
+        cy.visit("/syntax/");
+        cy.login();
+        cy.get("body").contains("Ask a cow to say something smart");
+    });
+})
+;
