dced467f173a09ea18f638ee61b64aea108999c5
Yauhen Shulitski <jsnjack@gmail.com>
2022-02-01 23:55:03 +0100

    Fix import

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
-import Notifications from "vue-notification";
+import Notifications from "@kyvg/vue3-notification";
 import axios from "axios";
 
 
