ae3972ce085386291c76e7adade7df73c407dfb3
Yauhen Shulitski <jsnjack@gmail.com>
2022-06-12 14:22:35 +0200

    Add tests for concurrency

---
+++ src/frontend/cypress/integration/45build_concurrency.js
@@ -0,0 +1,115 @@
+describe("Build page - concurrency", function () {
+    it("should run both builds concurrently", function () {
+        const jobName = "myjob" + new Date().getTime();
+        const jobContent = `
+desc: Env test
+tasks:
+  - name: Sleepy head
+    run: sleep 5
+
+concurrency: 0
+`;
+
+        cy.request({
+            url: "/api/job/" + jobName,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                fileContent: jobContent,
+            },
+            form: true,
+        });
+
+        // Create build
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+
+        cy.visit("/");
+        cy.login();
+        cy.get("[data-cy=filter]").clear().type(jobName);
+        cy.get("[data-cy=build-status-label]").should("have.length", 2);
+        cy.get("[data-cy=build-status-label]").should((items) => {
+            expect(items, "2 items").to.have.length(2);
+            expect(items.eq(0), "first item").to.contain("running");
+            expect(items.eq(1), "second item").to.contain("running");
+        });
+    });
+
+    it("should run only one build concurrently", function () {
+        const jobName = "myjob" + new Date().getTime();
+        const jobContent = `
+desc: Env test
+tasks:
+  - name: Sleepy head
+    run: sleep 5
+
+concurrency: 1
+`;
+
+        cy.request({
+            url: "/api/job/" + jobName,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {
+                fileContent: jobContent,
+            },
+            form: true,
+        });
+
+        // Create build
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+        cy.request({
+            url: `/api/job/${jobName}/run`,
+            method: "POST",
+            auth: {
+                user: "",
+                pass: "admin",
+            },
+            body: {},
+            form: true,
+        });
+
+        cy.visit("/");
+        cy.login();
+        cy.get("[data-cy=filter]").clear().type(jobName);
+        cy.get("[data-cy=build-status-label]").should("have.length", 2);
+        cy.get("[data-cy=build-status-label]").should((items) => {
+            expect(items, "2 items").to.have.length(2);
+            expect(items.eq(0), "first item").to.contain("pending");
+            expect(items.eq(1), "second item").to.contain("running");
+        });
+    });
+});
