9fe11ed0e9a25be0134c73b257af2242142fc1d8
Yauhen Shulitski <jsnjack@gmail.com>
2023-05-09 23:39:07 +0200

    Add new duration components

---
+++ src/frontend/src/components/SimpleDuration.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>{{ durationText }}</div>
+</template>
+
+<script>
+import { runningDuration, doneDuration, updateDurationPeriod } from "@/duration";
+import vuex from "vuex";
+
+export default {
+    props: {
+        item: {
+            required: true,
+            type: Object,
+        },
+    },
+    data: function () {
+        return {
+            updateInterval: null,
+            durationText: "",
+        };
+    },
+    computed: {
+        ...vuex.mapState(["durationMode"]),
+        isDone() {
+            switch (this.item.status) {
+                case "failed":
+                case "finished":
+                case "aborted":
+                case "skipped":
+                case "timed out":
+                    return true;
+            }
+            return false;
+        },
+    },
+    watch: {
+        "item.status": "onStatusChange",
+        "item.duration": "onStatusChange",
+        mode: "onStatusChange",
+    },
+    mounted() {
+        this.onStatusChange();
+    },
+    beforeUnmount: function () {
+        clearInterval(this.updateInterval);
+    },
+    methods: {
+        updateText() {
+            if (this.item.startedAt && this.item.startedAt.indexOf("0001-") === 0) {
+                // Go's way of saying it is zero
+                this.durationText = "";
+                return;
+            }
+            if (this.item.status === "running") {
+                this.durationText = runningDuration(this.item.startedAt);
+                return;
+            }
+            if (this.item.duration > 0) {
+                this.durationText = doneDuration(this.item.duration);
+                return;
+            }
+            return "";
+        },
+        onStatusChange() {
+            if (this.isDone) {
+                clearInterval(this.updateInterval);
+            } else if (this.item.status === "running" && !this.updateInterval) {
+                this.updateInterval = setInterval(
+                    function () {
+                        this.updateText();
+                    }.bind(this),
+                    updateDurationPeriod
+                );
+            }
+            this.updateText();
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/frontend/src/components/SimpleStartedAgo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>{{ startedText }}</div>
+</template>
+
+<script>
+import TimeAgo from "javascript-time-ago";
+
+import en from "javascript-time-ago/locale/en.json";
+
+TimeAgo.addDefaultLocale(en);
+const ago = new TimeAgo("en-US");
+
+export default {
+    props: {
+        item: {
+            required: true,
+            type: Object,
+        },
+    },
+    data: function () {
+        return {
+            startedText: "",
+        };
+    },
+    computed: {},
+    watch: {
+        "item.status": "updateText",
+    },
+    mounted() {
+        this.updateText();
+    },
+    methods: {
+        updateText() {
+            if (this.item.startedAt && this.item.startedAt.indexOf("0001-") === 0) {
+                // Go's way of saying it is zero
+                this.startedText = "";
+                return;
+            }
+            this.startedText = ago.format(new Date(this.item.startedAt));
+            return "";
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss"></style>
