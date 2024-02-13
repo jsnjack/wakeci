f77955d56c9cc4575fab1c3835a1f8cecbbeb1f7
Yauhen Shulitski <jsnjack@gmail.com>
2024-02-13 21:29:55 +0100

    :p: Use ESM syntax

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const path = require("path");
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
